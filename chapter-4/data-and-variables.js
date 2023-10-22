616656631a5f223baca1344ac91c15280ca283c4
Carolista <cjones.renaissancewoman@gmail.com>
2023-10-22 11:26:26 -0400

    set starter code for chapter 4 examples

---
+++ chapter-4/data-and-variables.js
@@ -0,0 +1,190 @@
+// LECTURE EXAMPLES
+// WEBDEV UNIT 1, CHAPTER 4: DATA & VARIABLES
+
+/* 
+	This is the STARTER CODE. Use this to code along with the lecture recording or just practice. You can switch to the solution branch if you want to reference the final code.
+
+	If Carrie is your primary instructor, 
+  you can locate the YouTube playlist for your cohort here:
+	https://youtube.com/@CodeWithCarrie
+*/
+
+
+/**** DATA TYPES ****/
+
+/* THE STRING */
+
+// TODO: Change the values for each string
+// Remember that quotes within a quote require using single quotes
+let emptyString = ""; // no characters
+let space = ""; // space only
+let zeeOrZed = ""; // single letter
+let farmBoy = ""; // single word
+let manInBlack = ""; // multiple words
+let memorableQuote = ''; // quotes inside quotes
+let rottenTomatoesScore = ""; // numbers, but as a string
+
+
+/* THE NUMBER */
+
+// TODO: Change the values for each number
+let numberOfOutlaws = 0; // integer
+let mostlyDead = 0; // decimal
+let energyLevelAfterRevival = 0; // negative
+
+
+/**** PRINTING TO THE CONSOLE ****/
+
+// TODO: Experiment with using the console directly.
+
+// TODO: print a string, then print a number
+
+// TODO: Hit the Run button (big green arrow) to see the output
+
+
+/* FORMATTING */
+
+// TODO: Add newline and tag characters to format an indented list
+// Un-comment the next line before running the program again
+// console.log('Fezzik & Inigo: "That Vizzini, he can fuss." "I think he like to scream at us." "Probably he means no harm." "He\'s really very short on charm."');
+
+/**** DETECTING TYPES & CONVERTING DATA ****/
+
+/* DETECTING TYPES */
+
+// TODO: Print the type of a string of characters to the console
+
+// TODO: Print the type of a negative number to the console
+
+
+/* CONVERTING DATA */
+
+// TODO: Convert 4 to a string and print the result, then verify the type
+
+// TODO: Convert "10000" to a number and print the result, then check the type
+
+// TODO: Try to convert "Humperdinck" to a number and print the result, then check the type
+
+
+/**** THE VARIABLE ****/
+
+// TODO: Declare & initialize a string variable, then print it
+
+// TODO: Declare & initialize a number variable, then print it
+
+// TODO: Give the newest variable a new value, then print it
+
+// TODO: Declare another variable and then print before initializing:
+
+// TODO: Initialize the variable, then print it again:
+
+
+/**** THE CONSTANT ****/
+
+// TODO: Declare and initialize a constant
+// Use the allcaps underscore naming convention
+
+// TODO: Try to assign a new value to the constant. 
+// What happens when you run the program?
+
+// Comment out the line so we won't keep seeing the error
+
+
+/**** WHAT'S IN A NAME? *****/
+
+// These are not great choices for variable names. 
+let book = 'The Princess Bride: S. Morgenstern\'s Classic Tale of True Love and High Adventure, The "Good Parts" Version';
+let name = "William Goldman";
+let type = "adventure";
+let kind = "hardcover";
+let books = 10;
+
+// TODO: Declare new variables for the values above
+// They should be descriptive, specific and obvious about data type
+
+
+/**** EVALUATING EXPRESSIONS WITH OPERATORS ****/
+
+/* Arithmetic */
+
+let a = 7;
+let b = 2;
+// TODO: Print the difference between a and b using the arithmetic operator for subtraction
+
+let m = 7
+// TODO: Print m to the 4th power using the exponentiation operator
+// In this case, create a variable to store the result, then print the variable name
+
+
+/* INCREMENT & DECREMENT */
+
+// Incrementing
+let x = 5;
+
+// TODO: Print x++, then print x, then print ++x
+// Try to guess the values that will result with each console.log()
+
+// TODO: Increment x one more time without printing it, then print x on the next line
+
+// Decrementing
+let y = 19;
+
+// TODO: Print y--, then print y, then print y--
+// Try to guess the values that will result with each console.log()
+
+// TODO: Decrement y one more time without printing it, then print y on the next line
+
+
+/* MODULO */
+
+// TODO: Print the remainder from 100 with a modulus of 7
+// 7 * 14 = 98, so we expect the value to be 2
+
+// TODO: Print the remainder from 6 with a modulus of 2
+// 2 * 3 = 6, so we expect the value to be 0
+
+
+/* ORDER OF OPERATIONS */
+
+// TODO: un-comment the line below and run the program to verify the result is 42
+// console.log(8 * (2 + 2**2) - 36 / (14 - 2**3));
+
+
+/* STRING BUILDING */
+
+let titleAfterMawage = "Princess";
+let originalName = "Buttercup";
+
+// TODO: Print the title and name together
+
+// TODO: What happens if you have a mix of strings and numbers? Try it out.
+
+
+/* COMPOUND ASSIGNMENT */
+
+let numberOfBoos = 3;
+
+// TODO: After Buttercup asks the Ancient Woman why she is booing her, she booed 5 more times. Increase the value of the variable, then print it.
+
+
+
+/**** GETTING USER INPUT ****/
+
+// TIP: During development, you play the role of user so you can test your own code
+
+// TODO: Import the readline-sync library as the constant input
+// Normally this would go at the top of the file
+
+// TODO: Print a greeting to the user
+
+// TODO: Ask the user for a name and store it in a variable
+
+// TODO: Print a response to the user that includes their input
+
+// TODO: Ask the user for another name and store it in a variable
+
+// TODO: Print a response to the user that includes their input
+
+// TODO: Go back and clean up the output using special characters so it is easier to read in the console
+
+// BONUS: After doing your prep work for class 2 (chapter 5), add logic to the final response to the user so that they see something different if their answer was incorrect
