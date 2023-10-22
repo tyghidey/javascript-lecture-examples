2dca0c009b2de5375022d99a2d2fdeb129757c69
Carolista <cjones.renaissancewoman@gmail.com>
2023-10-22 11:44:09 -0400

    added starter code for chapter 6 examples

---
+++ chapter-6/debugging.js
@@ -0,0 +1,25 @@
+// LECTURE EXAMPLES
+// WEBDEV UNIT 1, CHAPTER 6: DEBUGGING
+
+/* 
+	This is the STARTER CODE. Use this to code along with the lecture recording or just practice. You can switch to the solution branch if you want to reference the final code.
+
+	If Carrie is your primary instructor, 
+  you can locate the YouTube playlist for your cohort here:
+	https://youtube.com/@CodeWithCarrie
+*/
+
+
+/** DEBUGGING **/
+console.log("\nDebugging");
+
+// TODO: Un-comment the code below. What happens when you run the program?
+// TODO: Find and fix all 3 errors — let the error messages guide you on the first two; the third one is a logic error (test both true and false values for hadFirstKiss)
+
+let hadFirstKiss = true;
+
+// if ("hadFirstKiss") {
+// 	console.log("\nMarty McFly lives! He will not fade from existence.);
+// } else 
+// 	console.log("\nOh no! George and Lorraine didn't fall in love, and Marty McFly will never be born.");
+// }
