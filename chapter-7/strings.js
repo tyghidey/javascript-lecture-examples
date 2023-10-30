913f4e9be9e2aceedbc58b15bf116b3e243f66e7
Carolista <cjones.renaissancewoman@gmail.com>
2023-10-30 11:45:53 -0400

    modified TODOs for chapter 7 and 8 examples

--- chapter-7/strings.js
+++ chapter-7/strings.js
@@ -14,28 +14,28 @@
 
 let theLastTimeLord = " Tha Docktor  "; // um... that's not quite right!
 
-// Use bracket notation to print the first character of the first word.
+// TODO: Use bracket notation to print the first character of the first word.
 
 
-// Use bracket notation to print the first character of the last word.
+// TODO: Use bracket notation to print the first character of the last word.
 
 
-// Try to fix the mistakes in the name by using bracket notation to assign new values to characters at specific indices, then print the whole name to see if it worked.
+// TODO: Try to fix the mistakes in the name by using bracket notation to assign new values to characters at specific indices, then print the whole name to see if it worked.
 
 
 
 /** THE LENGTH PROPERTY **/
 
-// Declare a variable
+// TODO: Declare a variable
 
 
-// Print the length of the string.
+// TODO: Print the length of the string.
 
 
-// Use the length to store the index of the last character in a variable, then print it.
+// TODO: Use the length to store the index of the last character in a variable, then print it.
 
 
-// Use bracket notation and the variable you created to print the last character of the string.
+// TODO: Use bracket notation and the variable you created to print the last character of the string.
 
 
 
@@ -45,34 +45,34 @@ let theLastTimeLord = " Tha Docktor  "; // um... that's not quite right!
 let getAMoveOn = "Allons-y!";
 let timeDescription = "Wibbly-wobbly, Timey-wimey";
 
-// Print getAMoveOn in all caps, then print the stored variable.
+// TODO: Print getAMoveOn in all caps, then print the stored variable.
 
 
-// Print timeDescription in all lowercase, then print the stored variable.
+// TODO: Print timeDescription in all lowercase, then print the stored variable.
 
 
 
 /* Trim & Replace */
 
-// Remove the leading and trailing spaces from theLastTimeLord and reassign the returned value to the original variable, then print it.
+// TODO: Remove the leading and trailing spaces from theLastTimeLord and reassign the returned value to the original variable, then print it.
 
 
-// Change the errant a to an e in "Tha", reassigning to the original variable, then print it.
+// TODO: Change the errant a to an e in "Tha", reassigning to the original variable, then print it.
 
 
 
 /* Finding Characters Within a String */
 
-// Check to see if there is a t in theLastTimeLord, and print the return value (boolean).
+// TODO: Check to see if there is a t in theLastTimeLord, and print the return value (boolean).
 
 
-// Get the index of the errant k in the name, store it in a variable, then print it.
+// TODO: Get the index of the errant k in the name, store it in a variable, then print it.
 
 
 
 /* Create a New String from a Substring */
 
-// Use .slice() to build the correct string, omitting the k, and reassign it to the original variable. Print the final result.
+// TODO: Use .slice() to build the correct string, omitting the k, and reassign it to the original variable. Print the final result.
 
 
 
@@ -86,17 +86,17 @@ let quote1 = "Oi!";
 let quote2 = "The first 19 years Of my life, nothing happened. Nothing at all. Not ever. And then I met a man called The Doctor.";
 let quote3 = "All of space and time he promised me!";
 
-// Using template literals, combine each quote with the correct character so that they come out in the console formatted like this: 
+// TODO: Using template literals, combine each quote with the correct character so that they come out in the console formatted like this: 
 // "I am the Master and you will obey me." - The Master
 
 
 
 /* PUT IT ALL TOGETHER */
 
-// Add the readline-sync library so you can accept user input.
+// TODO: Add the readline-sync library so you can accept user input.
 
 
-// Ask the user for a first name
+// TODO: Ask the user for a first name
 
 
 // The user may not give you their name formatted as you need it to be
@@ -105,8 +105,8 @@ let quote3 = "All of space and time he promised me!";
 // Think it through — what order should you do the modifications in?
 // NOTE: There is more than one way to do this!
 
-// Modify the name to ensure the first letter is capitalized, the remaining letters are lowercase, and no extra spaces are before or after the name.
+// TODO: Modify the name to ensure the first letter is capitalized, the remaining letters are lowercase, and no extra spaces are before or after the name.
 
 
-// Use a template literal to print a greeting with the name
+// TODO: Use a template literal to print a greeting with the name
 
--- chapter-8/arrays.js
+++ chapter-8/arrays.js
@@ -13,28 +13,28 @@
 
 let enemies = ["Daleks", "Cybermen", "the ones in the library", "Weeping Angels"];
 
-// Use bracket notation to print the second element of the array. Remember that arrays are zero-indexed!
+// TODO: Use bracket notation to print the second element of the array. Remember that arrays are zero-indexed!
 
 
-// Unlike strings, arrays ARE mutable — use bracket notation to replace the third element with the correct name, then print the entire array.
+// TODO: Unlike strings, arrays ARE mutable — use bracket notation to replace the third element with the correct name, then print the entire array.
 
 
-// Use bracket notation to add a new element to the array, the print the array. Calculate the next available index instead of hard-coding it
+// TODO: Use bracket notation to add a new element to the array, the print the array. Calculate the next available index instead of hard-coding it
 
 
-// Create an empty array, then use bracket notation to add the names of some of the Doctor's allies, then print the array.
+// TODO: Create an empty array, then use bracket notation to add the names of some of the Doctor's allies, then print the array.
 
 
-// Try printing an element of allies at index 10. What happens?
+// TODO: Try printing an element of allies at index 10. What happens?
 
 
 
 /** THE LENGTH PROPERTY **/
 
-// Print the length of the array of allies.
+// TODO: Print the length of the array of allies.
 
 
-// Use the length to calculate the index of the last element of the array of enemies, and print that element.
+// TODO: Use the length to calculate the index of the last element of the array of enemies, and print that element.
 
 
 
@@ -42,10 +42,10 @@ let enemies = ["Daleks", "Cybermen", "the ones in the library", "Weeping Angels"
 
 /* Locate an Element within an Array */
 
-// Check to see if Kate Stewart is in the array of allies and print the result (true or false).
+// TODO: Check to see if Kate Stewart is in the array of allies and print the result (true or false).
 
 
-// Find the index of the Weeping Angels in the enemies array and print it
+// TODO: Find the index of the Weeping Angels in the enemies array and print it
 
 
 
@@ -56,13 +56,13 @@ let companions11 = ["Amy", "Rory"];
 let companions12 = ["Clara", "Bill", "Nardole"];
 let companions13 = ["Ryan", "Graham", "Yasmin"];
 
-// Combine the three arrays into a single array and print it.
+// TODO: Combine the three arrays into a single array and print it.
 
 
 
 /* Create by Taking a Slice */
 
-// Create a new array from just the middle 3 of the list of companions, then print it.
+// TODO: Create a new array from just the middle 3 of the list of companions, then print it.
 
 
 
@@ -70,50 +70,57 @@ let companions13 = ["Ryan", "Graham", "Yasmin"];
 
 let judoonJS = "JO BLO VO BLO SO CO RO KRO PO TO";
 
-// Create a new array with each Judoon letter as a separate element, then print it
+// TODO: Create a new array with each Judoon letter as a separate element, then print it
+
 
 
 /* Joining an Array To Create a String */
 
-// Print the Judoon word as a string again, this time connected by something other than just a space
+// TODO: If you try to just "stringify" an array, it doesn't read very well.
+
+
+// TODO: Print the Judoon array as a string again, this time connected by something a comma and a space
+
+
+// TODO: Connect the array with something else
 
 
 
 /* Reverse */
 
-// Reverse the array with all enemies and print it
+// TODO: Reverse the array with all enemies and print it
 
 
 
 /* Sort */
 
-// Sort the companion list array alphabetically and print it.
+// TODO: Sort the companion list array alphabetically and print it.
 
 
 
 /* Add & Remove at End of Array */
 
-// Declare a variable to store the result when you remove the last element from the array of allies. Print the variable, then print the modified array.
+// TODO: Declare a variable to store the result when you remove the last element from the array of allies. Print the variable, then print the modified array.
 
 
-// Add the name back to the end of the array, then print the modified array.
+// TODO: Add the name back to the end of the array, then print the modified array.
 
 
 
 /* Add & Remove at Beginning of Array */
 
-// Declare a variable to store the result when you remove the first element from the list of enemies. Print the variable, then print the modified array.
+// TODO: Declare a variable to store the result when you remove the first element from the list of enemies. Print the variable, then print the modified array.
 
 
-// Add the removed element back to the beginning of the array, then print the modified array.
+// TODO: Add the removed element back to the beginning of the array, then print the modified array.
 
 
 /* Insert and/or Remove Anywhere in an Array */
 
-// Remove Harriet Jones from the allies array, then print the modified array.
+// TODO: Remove Harriet Jones from the allies array, then print the modified array.
 
 
-// Add Wilfred Mott to the list of allies, somewhere in the middle.
+// TODO: Add Wilfred Mott to the list of allies, somewhere in the middle.
 
 
 
@@ -121,14 +128,14 @@ let judoonJS = "JO BLO VO BLO SO CO RO KRO PO TO";
 
 let tardis = "tardis";
 
-// In a single line of code, change "tardis" to "T.A.R.D.I.S", then print the reassigned variable.
+// TODO: In a single line of code, change "tardis" to "T.A.R.D.I.S", then print the reassigned variable.
 // Steps, which may not be in the right order:
 //  split string into an array of characters
 //  make all characters uppercase
 //  join characters with periods
 
 
-// Starting with the array of enemies, print a single string that lists the enemies in reverse alphabetical order, each separated by a comma and space.
+// TODO: Starting with the array of enemies, print a single string that lists the enemies in reverse alphabetical order, each separated by a comma and space.
 // Steps, which must be done in a certain order:
 //  While it is an array, sort it, then reverse it
 //  Then join with the connector ", " to make it a string
@@ -136,11 +143,11 @@ let tardis = "tardis";
 
 /** MULTIDIMENSIONAL ARRAYS **/
 
-// Put all four arrays of companions inside a single array, then print it
+// TODO: Put all four arrays of companions inside a single array, then print it
 
 
-// Print Donna's name using bracket notation with allCompanions
+// TODO: Print Donna's name using bracket notation with allCompanions
 
 
-// Print Clara's name using bracket notation with allCompanions
+// TODO: Print Clara's name using bracket notation with allCompanions
 
