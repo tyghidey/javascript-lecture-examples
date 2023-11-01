0210beed5e29ca12ab1cd3358ec4e2da4da5cf35
Carolista <cjones.renaissancewoman@gmail.com>
2023-10-31 21:28:38 -0400

    added starter code for chapter 9 examples

---
+++ chapter-9/loops.js
@@ -0,0 +1,117 @@
+// LECTURE EXAMPLES
+// WEBDEV UNIT 1, CHAPTER 9: LOOPS
+
+/* 
+	This is the STARTER CODE. Use this to code along with the lecture recording or just practice. You can switch to the solution branch if you want to reference the final code.
+
+	If Carrie is your primary instructor, 
+  you can locate the YouTube playlist for your cohort here:
+	https://youtube.com/@CodeWithCarrie
+*/
+
+/** FOR LOOPS **/
+
+/* Incrementing the Iterator */
+console.log("\nIncrementing the Iterator");
+
+// Set up a loop that prints the numbers 5-10, one at a time
+
+
+// Do the same but start with i = 0
+
+
+
+/* Iterating over Characters of a String */
+console.log("\nIterating over Characters of a String");
+
+// Print each character one at a time
+let obiGreeting = "Hello there";
+
+
+
+/* Iterating over Elements of an Array */
+console.log("\nIterating over Elements of an Array");
+
+// Iterate over the array in reverse and print each element
+let ackbarQuote = ["trap!", "a", "It's"];
+
+
+
+
+/* Increments Other than 1 */
+console.log("\nIncrements Other than 1");
+
+// Print every other word using an increment of 2. Start with index 0, then run it again starting with 1.
+let quote = ["I'd", "I", "just", "have", "as", "a", "soon", "bad", "kiss", "feeling", "a", "about", "wookie", "this"];
+
+
+
+/* Using Conditionals within Loops */
+console.log("\nUsing Conditionals within Loops");
+
+// Find the ewoks, and print something when they're found
+let endorForest = "eowkewokeowekwokewokeowkeowekowewokokeowkeowkewokowk";
+
+
+
+/* The Accumulator Pattern */
+console.log("\nThe Accumulator Pattern");
+
+// Count up the moons. If something is not a moon, say so! Finally, print the total in a sentence.
+let moons = ["moon", "moon", "Death Star", "moon"];
+
+
+
+/* String Building */
+console.log("\nString Building");
+
+// A stormtrooper is chasing you and firing his E-11 Blaster Rifle, which of course is not hitting you AT ALL, but it makes a cool sound. Build the string "pew pew pew" one "pew" at a time
+
+
+
+/* Filtering an Array */
+console.log("\nFiltering an Array");
+
+// Create a new array that has only words beginning with "A" from the original array, then print it
+let names = ["Aldaraan", "Calrissian", "Andor", "Armorer", "Ahsoka", "Greedo", "Ackbar", "Sateen", "Amadala"];
+
+
+
+
+/** WHILE LOOPS **/
+console.log("\nWhile Loops");
+
+// Han is keeping track of the distance while making the Kessel Run... he's about halfway through... let's help him count down!
+
+
+
+
+
+
+/* Searching for Something */
+console.log("\nSearching for Something");
+
+// Help Mando with his new bounty
+let bounties = ["smuggler", "thief", "child", "thief", "smuggler", "murderer", "thief"];
+
+
+
+/* User Input Validation */
+console.log("\nUser Input Validation");
+
+const input = require('readline-sync');
+
+// Luke, Han, and Chewbacca are trying to rescue Leia, who is being held in cell #2187. Ask them for the cell block and if they are within the 2100-2200 blocks, tell them they're on the right track! Otherwise tell them to keep searching.
+
+
+
+/** Nested Loops **/
+console.log("\nNested Loops");
+
+// Make sure Princess Leia gets the Death Star plans, if you can find her!
+let characters = [
+	["Cassian Andor", "Jyn Erso", "K-2SO"],
+	["Chirrut Imwe", "Baze Malbus"],
+	["Bail Organa", "Princess Leia", "Mon Mothma", "Admiral Raddus"],
+	["Wedge Antilles", "Red Leader", "Gold Leader"],
+];
