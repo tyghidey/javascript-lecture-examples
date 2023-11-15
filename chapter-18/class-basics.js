f4183645d8da11ef657eb3cee7c240d6d3383d78
Carolista <cjones.renaissancewoman@gmail.com>
2023-11-15 13:52:51 -0500

    updated file structure for class 18 starter code

---
+++ chapter-18/class-basics.js
@@ -0,0 +1,27 @@
+// LECTURE EXAMPLES
+// WEBDEV UNIT 1, CHAPTER 18: CLASSES
+
+/* 
+	This is the STARTER CODE. Use this to code along with the lecture recording or just practice. You can switch to the solution branch if you want to reference the final code.
+
+	If Carrie is your primary instructor, 
+  you can locate the YouTube playlist for your cohort here:
+	https://youtube.com/@CodeWithCarrie
+*/
+
+// TODO: Import the Fruit class here for use below
+
+
+/* The Fruit Class */
+
+// TODO: In fruit.js, define the Fruit class before continuing
+
+// TODO: Create a Fuji apple, Granny Smith apple, and navel orange
+
+// TODO: Print the three new objects
+
+// TODO: Print the color of the fuji apple
+
+// TODO: Describe the navel orange
+
+// TODO: Cut both apples into different numbers of pieces
