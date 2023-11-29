a6c8c3ca78a4b7554564bd9c40f5555ac93914ad
Carolista <cjones.renaissancewoman@gmail.com>
2023-11-29 15:17:47 -0500

    added starter code for chapter 23 examples

---
+++ chapter-23/random-num/script.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", function () {
+
+  /* CREATE OBJECTS */
+
+  // Generally a good idea to declare objects outside of listeners so they are in scope for everything
+  // There are exceptions - like if the element isn't on the page yet because it gets added later
+  
+  // Object needed for TODO #1
+
+  // Objects needed for TODO #2
+
+
+
+  /* CODE TO RUN IMMEDIATELY */
+
+  // Run any code needed to display things on page when it first loads
+
+  // TODO #1 - Add to existing text using new Date object and .toLocaleDateString() method
+
+
+
+  /* EVENT LISTENERS */
+
+  // TODO #2 - Add a click event listener for the button to generate a random number
+  // Will also need to make the div holding the number visible
+
+  // TODO #3 - Change color of number to yellowgreen if even or coral if odd
+  // BONUS: Also change the page background to match (use document.body)
+
+  // TODO #4 - If the random number is evenly divisible by 3, add the spinning class to the button; otherwise remove it
+
+  // TODO #5 - Trigger alert if number is evenly divisible by 5
+  // NOTE: Use setTimeout() to deal with race condition; 50ms is plenty of time to delay
+
+
+  // As part of TODO #2: Take a look at object content using console.log and console.dir
+
+  
+});
