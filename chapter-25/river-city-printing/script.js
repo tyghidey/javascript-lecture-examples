2f8d318773a878628072e164917ecde8a5a50d75
Carolista <cjones.renaissancewoman@gmail.com>
2023-12-04 03:27:31 -0500

    added chapter 25 examples

---
+++ chapter-25/river-city-printing/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', function() {
+
+  // Elements in card preview to add/change content/styling
+  const cardBkg = document.getElementById('card-bkg');
+  const cardText = document.getElementById('card-text');
+  const name = document.getElementById('name');
+  const address1 = document.getElementById('address1');
+  const address2 = document.getElementById('address2');
+  const phoneEmail = document.getElementById('phone-email');
+
+  // TODO: Add object for form
+
+
+  // TODO: Add objects for input elements
+
+
+  // TODO: Add object for reset button
+  
+  
+  // TODO: Update styling of card preview based on input
+  // Card background - background-image and justify-content
+  // Card text - align-items and text color
+  // Name text - font-family and font-size
+
+
+  // TODO: Update content of card preview with user input
+
+
+  // TODO: Add logic for resetting form & card preview
+
+
+  // TODO: Give user an alert after a successful form submission
+  // This will run only after it passes built-in validation
+ 
+
+});
