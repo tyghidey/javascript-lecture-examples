9775d18cfad6145b0549fdefdb29f26be2bb8414
Carolista <cjones.renaissancewoman@gmail.com>
2024-03-25 00:02:48 -0400

    added chapter 3 examples

---
+++ chapter-3/console-practice.js
@@ -0,0 +1,11 @@
+console.log("Welcome to LaunchCode, everyone!");
+
+// TODO: open the bottom panel with Cmd-J (Mac) or Ctrl-J (PC) and go to the terminal
+// Use 'cd' into the chapter-3 directory
+// Run this file with the command 'node console-practice.js'
+// What do you see?
+
+// TODO: Use the newline character "\n" to print your city and state on two lines with one line of code
+// Put your code here
+
+// Run this file in the terminal below
