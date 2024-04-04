9323ef3484cf0259c373042020db29094b83aa2b
Carolista <cjones.renaissancewoman@gmail.com>
2024-04-04 00:03:59 -0400

    updated starter code for ch 11 examples

--- chapter-11/more-on-functions.js
+++ chapter-11/more-on-functions.js
@@ -53,25 +53,20 @@ let msg2 = [18, 21, 14];
 
 /** HIGHER-ORDER FUNCTIONS **/
 
-// TODO: Write a named, higher-order function called processEach that takes two parameters: an array and a function. It should loop through the array and execute the function for each element of the array. (We are writing our own version of JavaScript's built-in array method .forEach())
-
-
-/* Using an Existing Function as an Argument */
-
 // TODO: Write a function that will convert a number into a string that has a length of exactly 3, with leading zeros if the number is less than 3 digits long. The function should simply print the string and does not need a return value.
 
 
 // Test data
 let indigoSubjects = [1, 3, 4, 7, 8, 10, 11, 13, 15, 18];
 
-// TODO: Call the higher-order function and pass in the test array with the new function (by name reference) to have it print a line for every element in the array.
+// TODO: Call the built-in array method .forEach() and pass in the test array with the new function (by name reference) to have it print a formatted line for every element in the array.
 
 
 /* Providing an Anonymous Function as an Argument */
 
 let hellfireClub = ["Eddie", "Gareth", "Jeff", "Mike", "Dustin", "Lucas", "Erica"];
 
-// TODO: Call the higher-order function again, but this time write an anonymous function in place that prints "_____ is a member of the Hellfire Club!" for each element in the array above... unless that person is Erica, in which case it should say "Lady Applejack, the MOST BADASS member" instead of "a member".
+// TODO: Use .forEach() again, but this time write an anonymous function in place that prints "_____ is a member of the Hellfire Club!" for each element in the array above... unless that person is Erica, in which case it should say "Lady Applejack, the MOST BADASS member" instead of "a member".
 
 
 
