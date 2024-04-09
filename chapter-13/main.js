f49d7ce8cfe64af9f9ad35e4129ffdc6f9620c5a
Carolista <cjones.renaissancewoman@gmail.com>
2024-04-09 01:13:52 -0400

    updated starter code for chapter 13 examples

--- chapter-13/main.js
+++ chapter-13/main.js
@@ -3,11 +3,12 @@
 
 
 // NOTE: Import statements usually go together at the top but I will add them gradually below
+const input = require('readline-sync');
 
 
 /** EXPORT/IMPORT A SINGLE ITEM **/
 
-// TODO: Import the function from contact.js that prints LaunchCode's organizational information, but give it a different name 
+// TODO: Import the function from contact.js that prints ACME's information, but give it a different name
 
 // TODO: Call the function
 
@@ -16,22 +17,10 @@
 
 // TODO: Import from utils.js using an alias for the exports object
 
-
 let longDecNum = 43.624571248419348;
 
 // TODO: Use the rounding function in utils to print the rounded number to 3 decimal places
 
-
-
-/** USING DECONSTRUCTION FOR IMPORTS **/
-
-// TODO: Use deconstruction to import only the last two functions from utils.js
-
-
-// TODO: Use deconstruction to import the question function from the readline-sync library
-
-
 // TODO: Print a random integer between 0 and 100
-
 
 // TODO: Ask a user for a book title, then print it in title case using the third function from utils.js
