0741df2acc377c0df6c4784722a1786f09909bb8
Carolista <cjones.renaissancewoman@gmail.com>
2024-04-14 01:00:37 -0400

    updated TODOs for ch 23

--- chapter-23/intro/script.js
+++ chapter-23/intro/script.js
@@ -1,20 +1,22 @@
 window.addEventListener('load', function() {  
 
-  // TODO: Take a look at native global objects, window and document
-    
+  // TODO: Log window
+
   // TODO: Use window.alert()
 
   // TODO: Use confirm() - without appending to window 
+
+  // TODO: Use both console.log and console.dir to see document
   
   
   const intro = document.getElementById('intro');
 
   // Always available because it comes from browser's current version of the document
-  console.log(intro.innerHTML);
-  console.log(intro.style.fontSize);
+  // console.log(intro.innerHTML);
+  // console.log(intro.style.fontSize);
 
   // Not available yet because JS can't read the CSS file - it can only read inline
-  console.log(intro.style.color);
+  // console.log(intro.style.color);
   
   // TODO: Change the values of the text, color, and font size
 
