f91d36b6188e415d9286d52fb75b6bbb21ffb33e
tyghidey <109488465+tyghidey@users.noreply.github.com>
2024-11-10 11:13:17 -0600

    completed chapter 7 examples

--- chapter-7/strings.js
+++ chapter-7/strings.js
@@ -7,28 +7,30 @@
 let theLastTimeLord = " Tha Docktor  "; // um... that's not quite right!
 
 // TODO: Use bracket notation to print the first character of the first word.
-
+console.log(theLastTimeLord[1]);
 
 // TODO: Use bracket notation to print the first character of the last word.
-
+console.log(theLastTimeLord[5]);
 
 // TODO: Try to fix the mistakes in the name by using bracket notation to assign new values to characters at specific indices, then print the whole name to see if it worked.
-
+theLastTimeLord[3] = "e"; //will not work
+console.log(theLastTimeLord);
 
 
 /** THE LENGTH PROPERTY **/
 
 // TODO: Declare a variable
-
+let dalekWarCry = "Exterminate!";
 
 // TODO: Print the length of the string.
-
+console.log(dalekWarCry.length);
 
 // TODO: Use the length to store the index of the last character in a variable, then print it.
-
+let lastIndex = dalekWarCry.length - 1;
+console.log(lastIndex); // prints a number
 
 // TODO: Use bracket notation and the variable you created to print the last character of the string.
-
+console.log(dalekWarCry[lastIndex]); // prints the character
 
 
 /** COMMON STRING METHODS **/
@@ -38,34 +40,42 @@ let getAMoveOn = "Allons-y!";
 let timeDescription = "Wibbly-wobbly, Timey-wimey";
 
 // TODO: Print getAMoveOn in all caps, then print the stored variable.
-
+console.log(getAMoveOn.toUpperCase());
+console.log(getAMoveOn);
 
 // TODO: Print timeDescription in all lowercase, then print the stored variable.
 
-
+console.log(timeDescription.toLowerCase());
+console.log(timeDescription);
 
 /* Trim & Replace */
 
 // TODO: Remove the leading and trailing spaces from theLastTimeLord and reassign the returned value to the original variable, then print it.
-
+theLastTimeLord = theLastTimeLord.trim();
+console.log(theLastTimeLord);
 
 // TODO: Change the errant a to an e in "Tha", reassigning to the original variable, then print it.
 
-
+theLastTimeLord = theLastTimeLord.replace("a", "e");
+console.log(theLastTimeLord);
 
 /* Finding Characters Within a String */
 
 // TODO: Check to see if there is a t in theLastTimeLord, and print the return value (boolean).
 
+console.log(theLastTimeLord.includes("t"));
 
 // TODO: Get the index of the errant k in the name, store it in a variable, then print it.
 
-
+let extraLetter = theLastTimeLord.indexOf("k");
+console.log(extraLetter);
 
 /* Create a New String from a Substring */
 
 // TODO: Use .slice() to build the correct string, omitting the k, and reassign it to the original variable. Print the final result.
 
+theLastTimeLord = theLastTimeLord.slice(0, extraLetter) +theLastTimeLord.slice(extraLetter + 1);
+console.log(theLastTimeLord);
 
 
 /** TEMPLATE LITERALS **/
@@ -80,15 +90,19 @@ let quote3 = "All of space and time he promised me!";
 
 // TODO: Using template literals, combine each quote with the correct character so that they come out in the console formatted like this: 
 // "I am the Master and you will obey me." - The Master
-
-
+console.log(`"${quote1}" - "${donna}"`);
+console.log(`"${quote2}" - "${rose}"`);
+console.log(`"${quote3}" - "${martha}"`);
 
 /* PUT IT ALL TOGETHER */
 
 // TODO: Add the readline-sync library so you can accept user input.
 
+const input = require('readline-sync');
 
 // TODO: Ask the user for a first name
+
+let firstName = input.question("\nPlease enter your first name:\n");
 
 
 // The user may not give you their name formatted as you need it to be
@@ -99,6 +113,11 @@ let quote3 = "All of space and time he promised me!";
 
 // TODO: Modify the name to ensure the first letter is capitalized, the remaining letters are lowercase, and no extra spaces are before or after the name.
 
+firstName = firstName.trim().toLowerCase();
+//firstName = firstName.replace(firstName[0], firstName[0].toUpperCase())
+//another way to do this
+firstName = firstName[0].toUpperCase() + firstName.slice(1).toLowerCase();
+
 
 // TODO: Use a template literal to print a greeting with the name
-
+console.log(`Hello, ${firstName}`);
