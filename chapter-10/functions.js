7f3da00a738a256f974cbb5f9abcbb321b9e5363
tyghidey <109488465+tyghidey@users.noreply.github.com>
2024-11-29 10:29:37 -0600

    completed ch 10 examples

--- chapter-10/functions.js
+++ chapter-10/functions.js
@@ -34,30 +34,49 @@ let inventory = [];
 
 // TODO: Write a function that adds an item to Link's inventory and announces it
 
+function addItemToInventory(item) {
+    inventory.push(item);
+    console.log(`Link now has a ${item}!`);
+}
 
 // TODO: Add a sword and a shield to inventory, then print the inventory array
-
-
+addItemToInventory("sword");
+addItemToInventory("shield");
+console.log(inventory);
 
 // TODO: Get the player name from the user in the console
+let player = input.question("Please enter your name: ");
 
 // TODO: Write a function to greet the player with a message that can change
-
+function greetPlayer(message) {
+    let greeting = `${message}, ${player}!`
+    console.log(greeting);
+}
 
 // TODO: Call the function and pass in a message 
+greetPlayer("Good morning");
 
 // TODO: Check the scope of player
+console.log(player);
 
 // TODO: Check the scope of message and greeting
-
+//console.log(message);
+//console.log(greeting);
 
 let name = "Zelda";
 
 // TODO: Write a function to print a sentence when you first encounter a character
 // Use a parameter, name
 
+function announceCharacter(name) {
+    let introduction = `You meet ${name} as you walk through the door`;
+    console.log(introduction);
+}
 // TODO: Call the function with the variable name as the argument
+announceCharacter(name);
 
 // TODO: Call the function with a hard-coded string
+announceCharacter("Ganon");
 
 // TODO: Print the variable name
+console.log(name);
